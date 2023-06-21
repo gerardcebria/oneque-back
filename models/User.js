e8a8ef4648e5bfdec20374d740358f618c5446ea
Gerard Cebria <gerard.cebrialeon@luminis.eu>
2023-06-21 15:26:10 +0200

    insert users

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 // import connection
+import { query } from "express";
 import db from "../config/database.js";
   
 // Get All Users
@@ -27,7 +28,8 @@ export const getUserById = (id, result) => {
   
 // Insert User to Database
 export const insertUser = (data, result) => {
-    db.query("INSERT INTO user SET ?", [data], (err, results) => {             
+    const query = "INSERT INTO user (name, lastname, email, phone, queue) VALUES (?, ?, ?, ?, ?)";
+    db.query(query, [data.name, data.lastname, data.email, data.phone, data.queue], (err, results) => {             
         if(err) {
             console.log(err);
             result(err, null);
@@ -39,7 +41,7 @@ export const insertUser = (data, result) => {
   
 // Update User to Database
 export const updateUserById = (data, id, result) => {
-    db.query("UPDATE colas SET nombre = ?, fecha = ? WHERE id = ?", [data.product_name, data.product_price, id], (err, results) => {             
+    db.query("UPDATE user SET name = ?, lastname = ? WHERE id = ?", [data.product_name, data.product_price, id], (err, results) => {             
         if(err) {
             console.log(err);
             result(err, null);
